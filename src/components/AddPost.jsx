1a8b4407ceb04a802c173de8f90c6fa8e1b59a01
Sitt Thiha Naing <sixxxxthihanaing@gmail.com>
2024-04-11 18:09:58 +0700

    updated

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,26 +1,29 @@
 import { CreatePost } from "../api/PostAPI";
 import PostForm from "./PostForm";
-import { useMutation } from "@tanstack/react-query";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const AddPost = () => {
+  const queryClient = useQueryClient();
   //Mutation function
-  const CreatePostMutation = useMutation ({
-    mutationFn: CreatePost 
-  })
+  const CreatePostMutation = useMutation({
+    mutationFn: CreatePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+      console.log("Success");
+    },
+  });
 
-const handleSubmit = (post) => {
-  CreatePostMutation.mutate({
-    id:Math.floor((Math.random() * 1000) + 1),
-    ...post
-  })
-}
-
+  const handleSubmit = (post) => {
+    CreatePostMutation.mutate({
+      id: Math.floor(Math.random() * 1000 + 1).toString(),
+      ...post,
+    });
+  };
 
   return (
     <div>
       <h2>Add new Post</h2>
-      <PostForm onSubmit={handleSubmit}/>
+      <PostForm onSubmit={handleSubmit} />
     </div>
   );
 };
